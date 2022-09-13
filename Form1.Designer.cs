d53e4732e238230926141e663425d2f1c5823b2a
User <User@LAPTOP-N55GPOPJ>
2022-09-13 20:27:15 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace TempConverterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox_celsius = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_fahrenheit = new System.Windows.Forms.TextBox();
+            this.textBox_kelvin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_celsius
+            // 
+            this.textBox_celsius.AcceptsReturn = true;
+            this.textBox_celsius.Location = new System.Drawing.Point(105, 60);
+            this.textBox_celsius.Name = "textBox_celsius";
+            this.textBox_celsius.Size = new System.Drawing.Size(100, 20);
+            this.textBox_celsius.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(88, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_fahrenheit
+            // 
+            this.textBox_fahrenheit.AcceptsReturn = true;
+            this.textBox_fahrenheit.Location = new System.Drawing.Point(105, 93);
+            this.textBox_fahrenheit.Name = "textBox_fahrenheit";
+            this.textBox_fahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.textBox_fahrenheit.TabIndex = 2;
+            // 
+            // textBox_kelvin
+            // 
+            this.textBox_kelvin.AcceptsReturn = true;
+            this.textBox_kelvin.Location = new System.Drawing.Point(105, 126);
+            this.textBox_kelvin.Name = "textBox_kelvin";
+            this.textBox_kelvin.Size = new System.Drawing.Size(100, 20);
+            this.textBox_kelvin.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Celsius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fahrenheit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kelvin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(40, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Temperature Converter App";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TempConverterApp.Properties.Resources.Thermometer;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(230, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 96);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(298, 214);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_kelvin);
+            this.Controls.Add(this.textBox_fahrenheit);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_celsius);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(314, 253);
+            this.MinimumSize = new System.Drawing.Size(314, 253);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Temperature Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_celsius;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_fahrenheit;
+        private System.Windows.Forms.TextBox textBox_kelvin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConverterApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double celsius = 0;
+        double fahrenheit = 0;
+        double kelvin = 0;
+        double tempval = 0;
+
+        String str_celsius = "";
+        String str_fahrenheit = "";
+        String str_kelvin = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox_celsius.Text != str_celsius)
+            {
+                // typing in celsius text box
+                tempval = double.Parse(textBox_celsius.Text);
+                fahrenheit = (tempval * 9 / 5) + 32;
+                textBox_fahrenheit.Text = fahrenheit.ToString();
+                str_fahrenheit = fahrenheit.ToString();
+                kelvin = (tempval + 273.15);
+                textBox_kelvin.Text = kelvin.ToString();
+                str_kelvin = kelvin.ToString();
+                str_celsius = textBox_celsius.Text;
+            }
+            if (textBox_fahrenheit.Text != str_fahrenheit)
+            {
+                // typing in farenheit text box
+                tempval = double.Parse(textBox_fahrenheit.Text);
+                celsius = (tempval - 32) * 5 / 9;
+                textBox_celsius.Text = celsius.ToString();
+                str_celsius = celsius.ToString();
+                kelvin = (tempval - 32) * 5 / 9 + 273.15;
+                textBox_kelvin.Text = kelvin.ToString();
+                str_kelvin = kelvin.ToString();
+                str_fahrenheit = textBox_fahrenheit.Text;
+            }
+            if(textBox_kelvin.Text != str_kelvin)
+            {
+                // typing in kelvin text box
+                tempval = double.Parse(textBox_kelvin.Text);
+                celsius = tempval - 273.15;
+                textBox_celsius.Text = celsius.ToString();
+                str_celsius = celsius.ToString();
+                fahrenheit = (tempval - 273.15) * 9 / 5 + 32;
+                textBox_fahrenheit.Text = fahrenheit.ToString();
+                str_fahrenheit = fahrenheit.ToString();
+                str_kelvin = textBox_kelvin.Text;
+            }
+ 
+        }
+
+        
+    }
+}
